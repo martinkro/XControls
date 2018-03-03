e17a080cb85a66d47bfc2087b31327a4ceebfacf
chameleon <martinkro@aliyun.com>
2018-03-03 20:00:11 +0800

    updater:progressbar

--- XControls/Updater/main.cpp
+++ XControls/Updater/main.cpp
@@ -1,9 +1,27 @@
 #include "Updater.h"
+#include "XControls/XApplication.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QString logFileName = "updater.log";
+
+#ifdef QT_DEBUG
+    QString qssName = "updater.qss";
+    QString translatorName = "updater_zh.qm";
+#else
+    QString qssName = ":/updater/stylesheet";
+    QString translatorName = ":/updater/language_zh";
+#endif
+
+
+
+    XApplication a(argc, argv);
+    a.setLogFileName(logFileName);
+    a.setQssName(qssName);
+    a.setTranslatorName(translatorName);
+    a.initialize();
+
     Updater w;
     w.show();
     return a.exec();
