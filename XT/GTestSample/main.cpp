fc7b5fbd1bd95e494c06e843c812da43215a9604
sundayliu <martinkro@aliyun.com>
2018-03-13 15:23:43 +0800

    googletest 1.8.0

---
+++ XT/GTestSample/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello" << endl;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
