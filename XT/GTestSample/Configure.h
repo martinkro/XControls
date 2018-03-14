1582cc4aed28c43673db85824663b23a132c61f9
sundayliu <martinkro@aliyun.com>
2018-03-14 20:00:17 +0800

    gtest example

---
+++ XT/GTestSample/Configure.h
@@ -0,0 +1,17 @@
+#pragma once 
+
+#include <string> 
+#include <vector> 
+
+class Configure
+{
+private:
+    std::vector<std::string> vItems;
+
+public:
+    int addItem(std::string str);
+
+    std::string getItem(int index);
+
+    int getSize();
+};
--- XT/GTestSample/main.cpp
+++ XT/GTestSample/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 #include "gtest/gtest.h"
+double cubic(double d)
+{
+    return pow(d, 3);
+}
+
+TEST(testMath, myCubeTest)
+{
+    EXPECT_EQ(1000, cubic(10));
+}
 
 int main(int argc, char* argv[])
 {
